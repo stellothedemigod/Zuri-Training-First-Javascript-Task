7e3c593cd9883803a45232998550265a1a1ee69d
stellothedemigod <divineodigili@gmail.com>
2022-06-25 04:58:02 -0700

    added html and js files

---
+++ printname.js
@@ -0,0 +1,11 @@
+// let firstname, height, country;
+// firstname= Divine; 
+// height= 5-feet-8-inches; 
+// country =Nigeria;
+
+// console.log ({fullname});
+// console.log ({height});
+// console.log ({country});
+console. log('Name : Divine Odigili')
+console. log('Height : 5.8 feet')
+console. log('Country : Nigeria')
